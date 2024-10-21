a3df1b7fe3d2b20edfdc9cabd9d2e2c4bf175172
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2024-10-21 17:46:28 -0300

    Revert "feat: merge-scriptable-objects"
    This reverts commit a20c06b63f6306c7735778635a1a3d2a722d6a95.

--- Assets/Scripts/Model/EventCard.cs
+++ Assets/Scripts/Model/EventCard.cs
@@ -2,35 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName="Event Card", menuName ="Event Cards")]
-public class EventCard : ScriptableObject
+public class EventCard : MonoBehaviour
 {
-    [Header("Introduction attributes")]
-    [SerializeField] string cardName;
-    [SerializeField] string cardType;
-    [SerializeField] string cardDescription;
-
-    [SerializeField] Sprite artwork;
-    [SerializeField] Sprite cardImage;
-
-    [Header("Decision-making attributes")]
-    [SerializeField] string totalSuccessText;
-    [SerializeField] string successText;
-    [SerializeField] string failureText;
-
-    [Header("Stats Modifiers")]
-    [SerializeField] int oxygenModifier;
-    [SerializeField] int healthModifier;
-    [SerializeField] int totalSuccessModifier;
-    [SerializeField] int successModifier;
-    [SerializeField] int failureModifier;
-
-    public string CardName { get => cardName; set => cardName = value; }
-    public string CardDescription { get => cardDescription; set => cardDescription = value; }
-    public string CardType { get => cardType; set => cardType = value; }
-    public Sprite Artwork { get => artwork; set => artwork = value; }
-    public Sprite CardImage { get => cardImage; set => cardImage = value; }
-    public string TotalSuccessText { get => totalSuccessText; set => totalSuccessText = value; }
-    public string SuccessText { get => successText; set => successText = value; }
-    public string FailureText { get => failureText; set => failureText = value; }
+    string optionType;
 }
